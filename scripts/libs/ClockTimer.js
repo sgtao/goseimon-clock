61ac1f04c8c8a34c4c8b9673372ccc42782deefb
Shogo Ogami <sg.tao.so@gmail.com>
2021-06-27 16:47:07 +0900

    split script to main and ClockTimer class

---
+++ scripts/libs/ClockTimer.js
@@ -0,0 +1,98 @@
+/* ClockTimer.js */
+class ClockTimer {
+  constructor(el_timer, el_clock, el_ctrl) {
+    this.elm_timer = document.querySelector(el_timer);
+    this.elm_clock = document.querySelector(el_clock);
+    this.elm_ctrl = document.querySelector(el_ctrl);
+    this.start_btn = this.elm_ctrl.querySelector('.start');
+    this.stop_btn = this.elm_ctrl.querySelector('.stop');
+    this.timerId = null;
+    this.isTimerRunning = false;
+    this.startTime = 0;
+    this.startTime_str = '0:0:0.00';
+    this.elapsedTime = 0;
+    // initial state timer and clock
+    this.initialize();
+  }
+  initialize() {
+    let self = this;
+    self.elm_timer.textContent = '0:0:0.00';
+    self.start_btn.addEventListener('click', function () {
+      if (self.isTimerRunning === true) { return; }
+      self.toggleStartStop();
+    });
+    self.stop_btn.addEventListener('click', function () {
+      if (self.isTimerRunning === false) { return; }
+      self.toggleStartStop();
+    });
+    // show digital-clock at Page Top
+    window.setInterval(function () {
+      self.show_clock();
+    }, 500);
+  }
+  show_clock() {
+    let dd = new Date();
+    // let options = { time: true, weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    // this.elm_clock.innerHTML = dd.toLocaleString('ja-JP', options);
+    this.elm_clock.innerHTML = dd.toLocaleString('ja-JP');
+  }
+  countmsec2time_str(msec_count) {
+    let time_str = "";
+    let total_sec = msec_count / 1000;
+    let sec = Math.floor(total_sec) % 60;
+    let min = Math.floor(total_sec / 60) % 60;
+    let hrs = Math.floor(total_sec / 3600) % 24;
+    // let msec = (msec_count % 1000);
+    let msec = ('00' + (msec_count % 1000)).slice(-2);
+    time_str = hrs + ':' + min + ':' + sec + '.' + msec;
+    // console.log(time_str);
+    return time_str;
+  }
+  updateTimerText() {
+    // Update Timer
+    let elapsedTime = Date.now() - this.startTime;
+    let elapsedTime_str = this.countmsec2time_str(elapsedTime);
+    this.elm_timer.textContent = elapsedTime_str + ' from ' + this.startTime_str;
+  }
+  /*
+      method  : タイマー開始
+      refer to https://yyengine.jp/blog/javascript/javascript-timer/
+  */
+  startTimer() {
+    let dd = new Date();
+    this.start_btn.classList.add('is-hidden');
+    this.stop_btn.classList.remove('is-hidden');
+    this.elm_clock.className = 'clock';
+    this.elm_timer.className = 'running';
+    this.startTime_str = dd.toLocaleTimeString();
+    console.log("startTime: " + this.startTime_str);
+    var self = this;
+    self.startTime = Date.now();
+    self.timerId = setInterval(function () {
+      self.updateTimerText();
+    }, 20);
+  }
+  /*
+      method  : タイマー停止
+  */
+  stopTimer() {
+    this.start_btn.classList.remove('is-hidden');
+    this.stop_btn.classList.add('is-hidden');
+    this.elm_clock.className = 'clock';
+    this.elm_timer.className = 'result';
+    let dd = new Date();
+    console.log("stopTime: " + dd.toLocaleTimeString());
+    clearInterval(this.timerId);
+  }
+  // button control
+  toggleStartStop() {
+    if (this.isTimerRunning === false) {
+      this.isTimerRunning = true;
+      this.startTimer();
+    } else {  // Start to Stop procedure
+      let resultTime, diff;
+      this.isTimerRunning = false;
+      this.stopTimer();
+    }
+  }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,203 +1,22 @@
 'use strict'
 
-function TimerWatchCountUp(startTime) {
-    let timerId = setTimeout(function () {
-      let elapsedTime = Date.now() - startTime;
-      // Update Watch
-      let total_sec = Math.floor(elapsedTime / 1000);
-      let sec = total_sec % 60;
-      let min = Math.floor(total_sec / 60) % 60;
-      let hrs = Math.floor(total_sec / 3600);
-      let msec = this.elapsedTime % 1000;
-      let startTime_str = this.startTime;
-      msec = ('00' + msec).slice(-2);
-      this.watch.textContent = hrs + ':' + min + ':' + sec + '.' + msec + ' from ' + startTime_str;
-      // this();
-    }, 20);
-}
-
-
-/*
-    カウントダウンタイマー　コンストラクタ
-*/
-var timerCountDown = function (sec) {
-  this.sec = sec;
-  this.timerId = null;
-  console.log("timerCountDown: " + this.sec);
-}
-
-/*
-    class   : カウントダウンタイマー
-    method  : 秒を減らす
-*/
-timerCountDown.prototype.countTime = function () {
-  this.sec--;
-  console.log("countTime: " + this.sec);
-  if (this.sec <= 0) this.completeTime();
-}
-
-/*
-    class   : カウントダウンタイマー
-    method  : タイマー開始
-*/
-timerCountDown.prototype.startTime = function () {
-  console.log("startTime: " + this.sec);
-  var self = this;
-  this.timerId = setInterval(function () { self.countTime() }, 1000);
-}
-
-/*
-    class   : カウントダウンタイマー
-    method  : タイマー停止
-*/
-timerCountDown.prototype.stopTime = function () {
-  console.log("stopTime: " + this.sec);
-  clearInterval(this.timerId);
-}
-
-/*
-    class   : カウントダウンタイマー
-    method  : タイマー完了
-*/
-timerCountDown.prototype.completeTime = function () {
-  console.log("completeTime: " + this.sec);
-  clearInterval(this.timerId);
-}
-
-var cd = new timerCountDown(5);     //インスタンス生成
-cd.startTime();
-
-
-
-class TimerWatch {
-  constructor(el_timer, el_clock, el_ctrl) {
-    this.elm_timer = document.querySelector(el_timer);
-    this.elm_clock = document.querySelector(el_clock);
-    this.elm_ctrl = document.querySelector(el_ctrl);
-    this.timerId = null;
-    this.isTimerRunning = false;
-    this.startTime = 0;
-    this.startTime_str = '0:0:0.00';
-    this.elapsedTime = 0;
-    // initial state timer and clock
-    this.elm_timer.textContent = '0:0:0.00';
-  }
-  show_clock() {
-    let dd = new Date();
-    this.elm_clock.innerHTML = dd.toLocaleString();
-  }
-  countmsec2time_str(msec_count) {
-    let time_str = "";
-    let total_sec = msec_count / 1000;
-    let sec = total_sec % 60;
-    let min = Math.floor(total_sec / 60) % 60;
-    let hrs = Math.floor(total_sec / 3600) % 24;
-    // let msec = (msec_count % 1000);
-    let msec = ('00' + (msec_count % 1000));
-    // msec = msec.slice(-2);
-    return hrs + ':' + min + ':' + sec + '.' + msec;
-  }
-  updateTimerText() {
-    // Update Timer
-    let elapsedTime = Date.now() - this.startTime;
-    let elapsedTime_str = this.countmsec2time_str(elapsedTime);
-    this.elm_timer.textContent = elapsedTime_str + ' from ' + this.startTime_str;
-  }
-  /*
-      method  : タイマー開始
-      refer to https://yyengine.jp/blog/javascript/javascript-timer/
-  */
-  startTimer () {
-    let dd = new Date();
-    this.startTime_str = dd.toLocaleString();
-    this.startTime = Date.now();
-    console.log("startTime: " + this.startTime_str);
-    this.countupTimer();
-  }
-  countupTimer() {
-    var self = this;
-    this.timerId = setTimeout(function () {
-      self.updateTimerText();
-      self.countupTimer();
-    }, 20);
-  }
-  /*
-      method  : タイマー停止
-  */
-  stopTimer () {
-    console.log("stopTime: " + Date.toLocaleString());
-    clearInterval(this.timerId);
-  }
-
-}
-
-
-{
-  var start = document.getElementById('start');
-  var stop = document.getElementById('stop');
-  var clock = document.getElementById('digital-clock');
-  var watch = document.getElementById('stop-watch');
-  var startTime, elapsedTime, targetTime;
-  var startTime_str = "";
-  var isStarted = false;
-  var timerId;
-
-
-
-  function toggleStartStop() {
-    if (isStarted === false) { // Stop to Start procedure
-      isStarted = true;
-      let dd = new Date();
-      startTime = Date.now();
-      startTime_str = dd.toLocaleTimeString();
-      start.className = 'is-hidden';
-      stop.className = '';
-      clock.className = 'clock';
-      watch.className = 'running';
-      countUp();
-    } else {  // Start to Stop procedure
-      let resultTime, diff;
-      isStarted = false;
-      clearTimeout(timerId);
-      stop.className = 'is-hidden';
-      start.className = '';
-      clock.className = 'clock';
-      watch.className = 'result';
-    }
-  }
-
-  start.addEventListener('click', function () {
-    if (isStarted === true) { return; } 
-    toggleStartStop();
-  });
-
-  stop.addEventListener('click', function () {
-    if (isStarted === false) { return; }
-    toggleStartStop();
-  });
-
-  
-  /* window actions */
-  window.onload = function () {
-    // show digital-clock at Page Top
-    let timerwatch = new TimerWatch('#stop-watch', '#digital-clock', '.bottons');
-    // Short-cut Keymap definition
-    document.addEventListener('keypress', (event) => {
-      let keyName = event.key;
-      console.log("detect press" + `keydown:${keyName}`);
-      if ((keyName === "S")||(keyName === "s")) { 
-        if (timerwatch.isTimerRunning === false) {
-          timerwatch.isTimerRunning = true;
-          timerwatch.startTimer();
-        } else {
-          timerwatch.stopTimer();
-        }
+/* window actions */
+window.onload = function () {
+  // show digital-clock at Page Top
+  let clocktimer = new ClockTimer('#stop-watch', '#digital-clock', '.bottons');
+  // Short-cut Keymap definition
+  document.addEventListener('keypress', (event) => {
+    let keyName = event.key;
+    console.log("detect press" + `keydown:${keyName}`);
+    if ((keyName === "S") || (keyName === "s")) {
+      if (clocktimer.isTimerRunning === false) {
+        clocktimer.isTimerRunning = true;
+        clocktimer.startTimer();
+      } else {
+        clocktimer.isTimerRunning = false;
+        clocktimer.stopTimer();
       }
-    });
-    // show digital-clock at Page Top
-    window.setInterval(function () {
-      timerwatch.show_clock();
-    }, 500);
-  }
-
+    }
+  });
 }
+
